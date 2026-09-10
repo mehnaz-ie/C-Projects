3b1e603eace8c102044d3791dfb2b630fc62c572
Mehnaz <mehbubamehnaz@gmail.com>
2026-09-10 20:58:56 +0600

    Add files via upload

---
+++ basicCalculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main (){
+    int operation, a, b;
+   
+    printf ("Enter two integer value: ");
+    scanf ("%d %d", &a, &b);
+    
+    printf ("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
+    printf ("Which operation do you want to perform? Type (1-4): ");
+    scanf ("%d", &operation);
+
+    switch (operation){
+        case 1:
+        printf ("Sum: %d", a+b);
+        break;
+        case 2:
+        printf ("Difference: %d", a-b);
+        break;
+        case 3:
+        printf ("Product: %d", a*b);
+        break;
+        case 4:
+        printf ("Quotient: %d", a/b);
+        break;
+        default: 
+        printf ("Error! Enter a valid operation.");
+    }
+
+    return 0;
+}
